51a09140f951862ecb2533e0183770ebe4c59c85
Yichung Lo <Yichung_Lo@htc.com>
2024-02-19 20:12:21 +0800

    refactor: refine app.tsx code structure

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,36 +1,43 @@
 import { useNavigate } from 'react-router-dom'
 import 'material-symbols'
 
+const HeroImg = () => {
+  // prettier-ignore
+  const icons = [
+    'flag', 'flag', 'flag',
+    'flag', 'bomb', 'flag',
+    'flag', 'flag', 'flag',
+  ]
+
+  return (
+    <div className="w-fit grid grid-cols-3 gap-1">
+      {icons.map((v, i) => {
+        return (
+          <span key={i} className="material-symbols-rounded text-4xl">
+            {v}
+          </span>
+        )
+      })}
+    </div>
+  )
+}
+
 function App() {
   const navigate = useNavigate()
 
   return (
-    <div className="max-w-5xl mx-auto pt-20">
+    <div className="max-w-5xl mx-auto pt-20 flex flex-col gap-10 items-center">
       <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl text-center">
         Minesweeper
       </h1>
-      <div className="w-fit mx-auto pt-10">
-        <div className="w-fit grid grid-cols-3 gap-1">
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">bomb</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-          <span className="material-symbols-rounded text-4xl">flag</span>
-        </div>
-      </div>
-      <div className="w-fit mx-auto pt-10">
-        <button
-          onClick={() => {
-            navigate('/game', { state: { key: 'value' } })
-          }}
-        >
-          Start
-        </button>
-      </div>
+      <HeroImg />
+      <button
+        onClick={() => {
+          navigate('/game', { state: { key: 'value' } })
+        }}
+      >
+        Start
+      </button>
     </div>
   )
 }
